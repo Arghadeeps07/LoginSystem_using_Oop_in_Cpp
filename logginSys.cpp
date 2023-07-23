d0ba1950fe0a300d56ff8f3b219c11940904c43d
ARGHADEEP SARKAR <112500013+Arghadeeps07@users.noreply.github.com>
2023-07-23 12:34:32 +0530

    Add files via upload

---
+++ logginSys.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class User{
+    string name;
+    int age;
+    string username;
+    string password;
+    bool Islogin = false;
+public:
+    User(string name,int age,string username,string password){
+        this->name = name;
+        this->age = age;
+        this->username = username;
+        this->password = password;
+    }
+    void login(string username,string password){
+        if(this->username == username && this->password == password){
+            cout << "Login successfully"<< endl;
+            Islogin = true;
+        }
+        else
+            cout << "Login unsuccessfully"<< endl;
+
+    }
+    void logout(){
+        cout << "logout successfully"<< endl;
+        Islogin = false;
+    }
+    void showDetails(){
+        if(Islogin==true){
+        cout << "show name" << name << endl;
+        cout << "show age" << age << endl;
+        cout << "show username" << username << endl;
+        }
+        else
+            cout << "user is logged out"<< endl;
+    }
+
+};
+int main(){
+    User u1("Arghadeep",20,"argha@302","argha_007");
+    u1.login("argha@302","argha_007");
+    u1.logout();
+    u1.showDetails();
+    return 0;
+}
